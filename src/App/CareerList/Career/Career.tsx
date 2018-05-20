80b5500c1e97dc25524697f72d4f2ee6f3188eb5
Graham Pegg <toish@toi.sh>
2018-05-19 23:19:35 -0400

    Fixed images

--- src/App/CareerList/Career/Career.tsx
+++ src/App/CareerList/Career/Career.tsx
@@ -12,7 +12,7 @@ function Career(props: ICareerProps): JSX.Element {
   return (
     <div className="Career__wrapper">
       {props.showImage ? (
-        <img src={props.career.image} />
+        <div className="Career__image" style={ {backgroundImage: `url(${props.career.image})`} } />
       ) : undefined}
       <h1>{props.career.title}</h1>
       <p>{props.career.description}</p>
