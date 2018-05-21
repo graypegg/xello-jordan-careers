ec8af3c5abdb199bbc541a729d4a727270e1a08d
Graham Pegg <toish@toi.sh>
2018-05-21 13:31:45 -0400

    Got tests passing for CareerImage, kinda TDD woo

--- src/App/CareerList/Career/CareerImage/CareerImage.test.tsx
+++ src/App/CareerList/Career/CareerImage/CareerImage.test.tsx
@@ -6,15 +6,16 @@ import { ImageMode } from '../../../../consts'
 import CareerImage from './CareerImage'
 
 it('renders without crashing', () => {
-  shallow(<CareerImage image="" mode={ImageMode.High} />)
+  shallow(<CareerImage image="" />)
 })
 
 it('renders a low-res image', () => {
-  const wrapper = render(<CareerImage image="test" mode={ImageMode.Low} />)
-  expect(wrapper.find('.CareerImage__image').attr('style')).toBe(`background-image: url(test?quality=70&maxWidth=500)`)
+  const wrapper = render(<CareerImage image="test" />)
+  expect(wrapper.find('.CareerImage__image').attr('style')).toBe(`background-image:url(test?quality=70&maxWidth=500)`)
 })
 
 it('renders a high-res image', () => {
-  const wrapper = render(<CareerImage image="test" mode={ImageMode.High} />)
-  expect(wrapper.find('.CareerImage__image').attr('style')).toBe(`background-image: url(test)`)
+  const wrapper = shallow(<CareerImage image="test" />)
+  wrapper.find('.CareerImage__modeToggleButton').first().simulate('click')
+  expect(wrapper.find('.CareerImage__image').props().style).toEqual({backgroundImage: `url(test)`})
 })
--- src/App/CareerList/Career/CareerImage/CareerImage.tsx
+++ src/App/CareerList/Career/CareerImage/CareerImage.tsx
@@ -1,16 +1,44 @@
-import { ImageMode } from '../../../../types'
+import { ImageMode } from '../../../../consts'
 
 import * as React from 'react'
 
 interface ICareerImageProps {
-  image: string,
+  image: string
+}
+
+interface ICareerImageState {
   mode: ImageMode
 }
 
-class CareerImage extends React.Component<ICareerImageProps> {
+class CareerImage extends React.Component<ICareerImageProps, ICareerImageState> {
+  constructor (props: ICareerImageProps) {
+    super(props)
+
+    this.state = {
+      mode: ImageMode.Low
+    }
+
+    this.scaleImage = this.scaleImage.bind(this)
+    this.toggleMode = this.toggleMode.bind(this)
+  }
+
+  public scaleImage (image: string) {
+    return `${image}${this.state.mode === ImageMode.High ? '' : '?quality=70&maxWidth=500'}`
+  }
+
+  public toggleMode (): void {
+    this.setState({
+      mode: this.state.mode === ImageMode.High ? ImageMode.Low : ImageMode.High
+    })
+  }
+
   public render (): JSX.Element {
     return (
-      <div>d</div>
+      <div className="CareerImage__wrapper">
+        <div className="CareerImage__image" style={ { backgroundImage: `url(${this.scaleImage(this.props.image)})` } }>
+          <div className="CareerImage__modeToggleButton" onClick={this.toggleMode}>Toggle</div>
+        </div>
+      </div>
     )
   }
 }
