687000ed01b021d29548e4d1beb89859ca3bb247
Graham Pegg <toish@toi.sh>
2018-05-23 23:21:59 -0400

    caught up on tests for careers list, not very TDD, uuuugh

--- src/App/CareerList/CareerList.test.tsx
+++ src/App/CareerList/CareerList.test.tsx
@@ -61,3 +61,47 @@ it('creates pages according to pageLength prop', () => {
 
   expect(wrapper.find(Career).length).toBe(1)
 })
+
+describe('pageination link list', () => {
+  const careers: ICareer[] = [
+    {
+      title: 'Test1',
+      description: 'Test1',
+      notes: ['a1', 'b1'],
+      image: 'TestImage1',
+      id: 1
+    },
+    {
+      title: 'Test2',
+      description: 'Test2',
+      notes: ['a2', 'b2'],
+      image: 'TestImage2',
+      id: 2
+    },
+    {
+      title: 'Test3',
+      description: 'Test3',
+      notes: ['a3', 'b3'],
+      image: 'TestImage3',
+      id: 3
+    }
+  ]
+
+  const wrapper = shallow(<CareerList careers={careers} showImages={false} pageLength={1}/>)
+
+  it('renders the link list container', () => expect(wrapper.find('.CareerList__pagination').length).toBe(1))
+  it('renders the unselected links', () => expect(wrapper.find('.CareerList__paginationLink').length).toBe(2))
+  it('renders the single selected link', () => expect(wrapper.find('.CareerList__paginationLink--active').length).toBe(1))
+
+  it('clicking the selected link does nothing', () => {
+    wrapper.find('.CareerList__paginationLink--active').simulate('click')
+    expect(wrapper.state().onPage).toBe(0)
+    expect(wrapper.find(Career).first().props().career).toEqual(careers[0])
+  })
+
+  it('clicking a link that is unselected advances the list of careers', () => {
+    wrapper.find('.CareerList__paginationLink').last().simulate('click')
+    expect(wrapper.state().onPage).toBe(2)
+    expect(wrapper.find(Career).first().props().career).toEqual(careers[2])
+  })
+})
